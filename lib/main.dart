20301a58b7350235cd55e822393e299870d362f6
Jorge Reyes <jorgeart81dev@gmail.com>
2025-09-26 18:18:17 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:cinemapedia/config/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    );
+  }
+}
