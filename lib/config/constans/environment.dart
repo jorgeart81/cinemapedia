7959e9ec956d46cc226b027aa18abcf4c17541b7
Jorge Reyes <jorgeart81dev@gmail.com>
2025-09-29 19:12:47 -0500

    feat: Install dio and add THE_MOVIEDB_URL env

--- lib/config/constans/environment.dart
+++ lib/config/constans/environment.dart
@@ -2,5 +2,9 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
   static String theMovieDBKey =
-      dotenv.env['THE_MOVIEDB_KEY'] ?? "THE_MOVIEDB_KEY no existe";
+      dotenv.env['THE_MOVIEDB_KEY'] ??
+      (throw Exception('THE_MOVIEDB_KEY no existe'));
+  static String theMovieDBURL =
+      dotenv.env['THE_MOVIEDB_URL'] ??
+      (throw Exception('THE_MOVIEDB_URL no existe'));
 }
