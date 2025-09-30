b4b0b5af19e18accf686e8d1d29605a553a4d146
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2025-09-30 00:29:55 -0500

    feat: Add MoviesSlideshow

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_slideshow_provider.dart';
+import 'package:cinemapedia/presentation/widgest/movies/movies_slideshow.dart';
 import 'package:cinemapedia/presentation/widgest/shared/custom_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -32,6 +34,7 @@ class _HomeViewState extends ConsumerState<HomeView> {
   @override
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final moviesSlideshow = ref.watch(moviesSlideshowProvider);
 
     if (nowPlayingMovies.isEmpty) {
       return Center(child: CircularProgressIndicator());
@@ -39,16 +42,8 @@ class _HomeViewState extends ConsumerState<HomeView> {
 
     return Column(
       children: [
-        CustomAppbar(),
-        Expanded(
-          child: ListView.builder(
-            itemCount: nowPlayingMovies.length,
-            itemBuilder: (context, index) {
-              final movie = nowPlayingMovies[index];
-              return ListTile(title: Text(movie.title));
-            },
-          ),
-        ),
+        const CustomAppbar(),
+        MoviesSlideshow(movies: moviesSlideshow),
       ],
     );
   }
