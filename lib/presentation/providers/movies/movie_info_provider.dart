2b7646557d5c18125cffdf4b6b7eb0ff610abf71
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2025-10-02 14:40:38 -0500

    feat: Add movieInfoProvider

---
+++ lib/presentation/providers/movies/movie_info_provider.dart
@@ -0,0 +1,26 @@
+import 'package:cinemapedia/domain/movie.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/legacy.dart';
+
+typedef GetMovieCallback = Future<Movie> Function(int id);
+
+final movieInfoProvider =
+    StateNotifierProvider<MovieMapNotifier, Map<String, Movie>>((ref) {
+      final getMovieById = ref.watch(movieRepositoryProvider).getMovieById;
+
+      return MovieMapNotifier(getMovie: getMovieById);
+    });
+
+class MovieMapNotifier extends StateNotifier<Map<String, Movie>> {
+  GetMovieCallback getMovie;
+
+  MovieMapNotifier({required this.getMovie}) : super({});
+
+  Future<void> loadMovie(int id) async {
+    if (state[id.toString()] != null) return;
+
+    Movie movie = await getMovie(id);
+
+    state = {...state, '$id': movie};
+  }
+}
