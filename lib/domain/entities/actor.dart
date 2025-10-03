dd95f3ed6b7e25fbe26c720696f6ed42d290badc
Jorge Reyes <jorgeart81dev@gmail.com>
2025-10-03 15:55:10 -0500

    refactor

--- lib/domain/entities/actor.dart
+++ lib/domain/entities/actor.dart
@@ -1,7 +1,7 @@
 class Actor {
   final int id;
   final String name;
-  final String? profilePath;
+  final String profilePath;
   final String? character;
 
   Actor({
--- lib/presentation/providers/movies/movies_repository_provider.dart
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -1,7 +1,8 @@
+import 'package:cinemapedia/domain/repositories/movies_repository.dart';
 import 'package:cinemapedia/infrastructure/datasources/moviedb_datasource.dart';
 import 'package:cinemapedia/infrastructure/repositories/moviedb_repository_impl.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final movieRepositoryProvider = Provider((ref) {
+final movieRepositoryProvider = Provider<MoviesRepository>((ref) {
   return MoviedbRepositoryImpl(MoviedbDatasource());
 });
