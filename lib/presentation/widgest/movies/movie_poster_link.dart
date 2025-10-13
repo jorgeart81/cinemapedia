9eba3b16f6061c9196fddb39c3b882128bdc4a95
Jorge Reyes <jorgeart81dev@gmail.com>
2025-10-13 16:41:39 -0500

    Improve MoviePosterLink

--- lib/presentation/widgest/movies/movie_poster_link.dart
+++ lib/presentation/widgest/movies/movie_poster_link.dart
@@ -1,5 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:go_router/go_router.dart';
 
 class MoviePosterLink extends StatelessWidget {
@@ -11,9 +13,39 @@ class MoviePosterLink extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => context.push('/movie/${movie.id}'),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20),
-        child: Image.network(movie.posterPath),
+      child: _Poster(src: movie.posterPath),
+    );
+  }
+}
+
+class _Poster extends StatelessWidget {
+  const _Poster({required this.src});
+
+  final String src;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: AspectRatio(
+        aspectRatio: 2 / 3,
+        child: CachedNetworkImage(
+          fit: BoxFit.cover,
+          imageUrl: src,
+          progressIndicatorBuilder: (context, url, downloadProgress) => Center(
+            child: CircularProgressIndicator(
+              strokeWidth: 2,
+              value: downloadProgress.progress,
+            ),
+          ),
+          cacheManager: CacheManager(
+            Config(
+              'favoriesKey',
+              stalePeriod: const Duration(days: 7),
+              maxNrOfCacheObjects: 20,
+            ),
+          ),
+        ),
       ),
     );
   }
